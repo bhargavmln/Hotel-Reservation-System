881bfa6c402a82ba5e85595ee8927ff0f6bcbb1c
bhargavmln <bhargavmln@gmail.com>
2020-10-20 16:17:31 +0530

    [bhargavmln]add:ability to find cheapest hotel

--- src/main/java/com/capgemini/hotel/HotelReservation.java
+++ src/main/java/com/capgemini/hotel/HotelReservation.java
@@ -1,33 +1,55 @@
 package com.capgemini.hotel;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Scanner;
 
 public class HotelReservation {
-	private static Scanner sc = new Scanner(System.in);	
+	private static Scanner sc = new Scanner(System.in);
 	private static ArrayList<Hotel> hotelList = new ArrayList<>();
-	
+	private static Date checkin, checkout;
+
 	public ArrayList<Hotel> getHotelList() {
 		return hotelList;
 	}
-	
-	public static void addHotel(){
+
+	public static void addHotel() {
 		System.out.println("Enter the name of hotel:");
 		String name = sc.nextLine();
 		System.out.println("Enter the price:");
 		int price = sc.nextInt();
-		Hotel temp = new Hotel(name,price);
+		Hotel temp = new Hotel(name, price);
 		hotelList.add(temp);
 	}
-	
+
+	public static String findCheapestHotel() {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("ddMMMyyyy");
+		System.out.println("\nEnter the chekin and check out date is the following format (ddMMMyyyy),(ddMMMyyyy):\n");
+		String temp = sc.next();
+		String[] dates = temp.split(",");
+		try {
+			checkin = dateFormat.parse(dates[0]);
+			checkout = dateFormat.parse(dates[1]);
+		} catch (ParseException e) {
+			System.out.println("invalid checkin date");
+		}
+		long difference = checkout.getTime() - checkin.getTime();
+		int numDays = (int) (difference / (1000 * 60 * 60 * 24)) + 1;
+		Hotel cheapestHotel = hotelList.get(0);
+		for (Hotel h : hotelList) {
+			if (h.getPrice() < cheapestHotel.getPrice()) {
+				cheapestHotel = h;
+			}
+		}
+		int price = cheapestHotel.getPrice();
+		String hotelName = cheapestHotel.getName();
+		int totalAmt = (int) numDays * price;
+		System.out.println("\nCheapest Hotel for the given dates is " + hotelName + "\nTotal Rates: $" + totalAmt);
+		return hotelName;
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to Hotel Reservation");
-		Hotel lakeWood=new Hotel("Lakewood",110);
-		Hotel bridgeWood=new Hotel("Bridgewood",110);
-		Hotel ridgeWood=new Hotel("Ridgewood",110);
-		hotelList.add(lakeWood);
-		hotelList.add(bridgeWood);
-		hotelList.add(ridgeWood);
-		System.out.println(hotelList);
 	}
 }
--- src/test/java/com/capgemini/hotel/HotelReservationTest.java
+++ src/test/java/com/capgemini/hotel/HotelReservationTest.java
@@ -2,16 +2,22 @@ package com.capgemini.hotel;
 
 import org.junit.Assert;
 import org.junit.Test;
-
+import java.util.ArrayList;
 
 public class HotelReservationTest {
 
 	@Test
-	public void givendetailsOf3Hotels_WhenAddedToHotelList_SizeOfListIs3(){
-		
-		HotelReservation.addHotel("Lakewood",110,90);
-		HotelReservation.addHotel("Bridgewood",160,60);
-		HotelReservation.addHotel("Ridgewood",220,150);
-		Assert.assertEquals(3, HotelReservation.hotels.size());
+	public void givendetailsOf3Hotels_WhenAddedToHotelList_SizeOfListIs3() {
+		HotelReservation temp = new HotelReservation();
+		Hotel lakeWood = new Hotel("Lakewood", 110);
+		Hotel bridgeWood = new Hotel("Bridgewood", 160);
+		Hotel ridgeWood = new Hotel("Ridgewood", 220);
+		ArrayList<Hotel> list = temp.getHotelList();
+		list.add(lakeWood);
+		list.add(bridgeWood);
+		list.add(ridgeWood);
+		Assert.assertEquals(3, list.size());
+
+		Assert.assertEquals("Lakewood", temp.findCheapestHotel());
 	}
 }
