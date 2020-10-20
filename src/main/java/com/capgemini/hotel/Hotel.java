a8ae358c3093f7271b3463422b132f948dc86ae7
bhargavmln <bhargavmln@gmail.com>
2020-10-20 15:13:21 +0530

    [bhargavmln]add:ability to add hotel containing name and price

--- src/main/java/com/capgemini/hotel/Hotel.java
+++ src/main/java/com/capgemini/hotel/Hotel.java
@@ -2,44 +2,29 @@ package com.capgemini.hotel;
 
 public class Hotel {
 
-	String hotelName;
-	int regularWeekdayPrice;
-	int regularWeekendPrice;
-
-	public Hotel(String hotelName, int regularWeekdayPrice, int regularWeekendPrice) {
-		super();
-		this.hotelName = hotelName;
-		this.regularWeekdayPrice = regularWeekdayPrice;
-		this.regularWeekendPrice = regularWeekendPrice;
+	private String name;
+	private int price;
+	
+	public Hotel(String name, int price) {
+		this.setName(name);
+		this.setPrice(price);
 	}
-
-	public String getHotelName() {
-		return hotelName;
+	
+	public String getName() {
+		return name;
 	}
-
-	public void setHotelName(String hotelName) {
-		this.hotelName = hotelName;
+	public void setName(String name) {
+		this.name = name;
 	}
-
-	public int getRegularWeekdayPrice() {
-		return regularWeekdayPrice;
+	public int getPrice() {
+		return price;
 	}
-
-	public void setRegularWeekdayPrice(int regularWeekdayPrice) {
-		this.regularWeekdayPrice = regularWeekdayPrice;
+	public void setPrice(int price) {
+		this.price = price;
 	}
-
-	public int getRegularWeekendPrice() {
-		return regularWeekendPrice;
-	}
-
-	public void setRegularWeekendPrice(int regularWeekendPrice) {
-		this.regularWeekendPrice = regularWeekendPrice;
-	}
-
+	
 	@Override
-	public String toString() {
-		return "Hotel Name: " + hotelName + "\nRegular Weekday Rate: " + regularWeekdayPrice
-				+ "\nRegular Weekend Rate: " + regularWeekendPrice + "\n";
+	public String toString(){
+		return "Hotel Name: "+name+" Price: "+price;
 	}
 }
--- src/main/java/com/capgemini/hotel/HotelReservation.java
+++ src/main/java/com/capgemini/hotel/HotelReservation.java
@@ -1,19 +1,33 @@
 package com.capgemini.hotel;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Scanner;
 
 public class HotelReservation {
-
-	static List<Hotel> hotels = new ArrayList<Hotel>();
-
-	public static void main(String[] args) {
-
-		System.out.println("Welcome to Hotel Reservation Program");
-
+	private static Scanner sc = new Scanner(System.in);	
+	private static ArrayList<Hotel> hotelList = new ArrayList<>();
+	
+	public ArrayList<Hotel> getHotelList() {
+		return hotelList;
 	}
-
-	public static void addHotel(String hotelName, int weekEndRate, int weekDayRate) {
-		hotels.add(new Hotel(hotelName, weekDayRate,weekEndRate));
+	
+	public static void addHotel(){
+		System.out.println("Enter the name of hotel:");
+		String name = sc.nextLine();
+		System.out.println("Enter the price:");
+		int price = sc.nextInt();
+		Hotel temp = new Hotel(name,price);
+		hotelList.add(temp);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to Hotel Reservation");
+		Hotel lakeWood=new Hotel("Lakewood",110);
+		Hotel bridgeWood=new Hotel("Bridgewood",110);
+		Hotel ridgeWood=new Hotel("Ridgewood",110);
+		hotelList.add(lakeWood);
+		hotelList.add(bridgeWood);
+		hotelList.add(ridgeWood);
+		System.out.println(hotelList);
 	}
 }
